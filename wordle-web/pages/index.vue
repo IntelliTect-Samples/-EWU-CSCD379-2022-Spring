23924747f706f908b325934292e1b7051f6d3fdb
BenagainB <91215159+BenagainB@users.noreply.github.com>
2022-04-07 15:46:54 -0700

    Update index.vue

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -7,9 +7,9 @@
       </v-card>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the NEW Vuetify + Nuxt.js template
         </v-card-title>
-        <v-card-text>
+        <v-card-text> 
           <p>
             Vuetify is a progressive Material Design component framework for
             Vue.js. It was designed to empower developers to create amazing
