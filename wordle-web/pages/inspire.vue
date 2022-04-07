26ba6293a6591465110e9cbbfb83b80ff0180999
DakotaCondos <dakota242424@live.com>
2022-04-07 16:11:55 -0700

    added name to page

--- wordle-web/pages/inspire.vue
+++ wordle-web/pages/inspire.vue
@@ -4,6 +4,7 @@
       <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
+        {{name}}
         <footer>
           <small>
             <em>&mdash;John Johnson</em>
