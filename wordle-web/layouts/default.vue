cd7632f87c6789f428baad31ce54c4c738fcb55c
BenagainB <91215159+BenagainB@users.noreply.github.com>
2022-04-11 22:06:23 -0700

    added game.vue page with buttons

--- wordle-web/layouts/default.vue
+++ wordle-web/layouts/default.vue
@@ -81,6 +81,11 @@ export default {
           title: 'Inspire',
           to: '/inspire',
         },
+        {
+          icon: 'mdi-gamepad-variant',
+          title: 'Wordle',
+          to: '/game',
+        },
       ],
       miniVariant: false,
       right: true,
---
+++ wordle-web/pages/game.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-row>
+        <v-col>
+            <v-card>
+                <h1>Wordles with Turtles</h1>
+                <p> Yertle was a turtle who knew oh so many wordles. As a young turtle, he was a bit of a nerdle (he even wore a girtle!). Now he's a bit of a flirtle, though his looks make milk curdle, and his favorite scent is myrtle and his favorite color is purple. He calls his home a yurtle!</p>
+                <p> Yertle was a turtle who knew oh so many wordles. As a young turtle, he was a bit of a nerdle (he even wore a girtle!). Now he's a bit of a flirtle, though his looks make milk curdle, and his favorite scent is myrtle and his favorite color is purple. He calls his home a yurtle!</p>
+                <p> Yertle was a turtle who knew oh so many wordles. As a young turtle, he was a bit of a nerdle (he even wore a girtle!). Now he's a bit of a flirtle, though his looks make milk curdle, and his favorite scent is myrtle and his favorite color is purple. He calls his home a yurtle!</p>
+            </v-card>
+            <v-card-actions>
+                <v-btn color="primary" nuxt to="/"> Back to Main </v-btn>
+            </v-card-actions>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+  name: 'GamePage',
+}
+</script>
