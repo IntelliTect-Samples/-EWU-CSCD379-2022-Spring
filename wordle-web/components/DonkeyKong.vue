26c1c9e03bf45786784e43cae3b4c9f2f51ff7df
tnadalini <63377908+tnadalini@users.noreply.github.com>
2022-04-11 20:23:34 -0700

    mm monke

---
+++ wordle-web/components/DonkeyKong.vue
@@ -0,0 +1,10 @@
+<template>
+  <img class="DonkeyKong" alt="Donkey Kong" src="/donkeykong.webp">
+</template>
+
+<style>
+.DonkeyKong {
+  width: 360px;
+  height: 360px;
+}
+</style>
--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,5 +1,29 @@
 <template>
   <v-row justify="center" align="center">
-
+    <v-col cols="12" sm="8" md="6">
+      <v-card-title class="headline justify-center">
+        <h1>Monke</h1>
+      </v-card-title>
+      <v-card class="logo py-4 d-flex justify-center">
+        <DonkeyKong />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the game page! :)
+        </v-card-title>
+        <v-card-text>
+          <p>
+            "You saved me...why?" - Monkey
+            "mm...Monkey" - Master Oogway
+          </p>
+          <hr class="my-3" />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="secondary" nuxt to="/"> home </v-btn>
+          <v-btn color="primary" nuxt to="/game"> restart </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
   </v-row>
 </template>
