cd8f6d5bf6edaee69cbd3c08acb14316be965468
AntonioGomez12 <103075701+AntonioGomez12@users.noreply.github.com>
2022-04-12 23:18:59 -0700

    changes

--- wordle-web/scripts/letter.ts
+++ wordle-web/scripts/letter.ts
@@ -1,15 +1,14 @@
-export enum LetterStatus {
-  Unknown = 0,
-  Correct = 1,
-  WrongPlace = 2,
-  Wrong = 3,
+export enum LetterStatus{
+    Unknown = 0,
+    Correct = 1,
+    WrongPlace = 2,
+    Wrong = 3
 }
 
-export class Letter {
-  constructor(char: string) {
-    this.char = char
-  }
-
-  char: string
-  status: LetterStatus = LetterStatus.Unknown
+export class Letter{
+    constructor(char:string){
+        this.char = char;
+    }
+    char:string;
+    status: LetterStatus = LetterStatus.Unknown;
 }
--- wordle-web/scripts/word.ts
+++ wordle-web/scripts/word.ts
@@ -1,58 +1,53 @@
-import { Letter, LetterStatus } from '~/scripts/letter'
+import{ Letter, LetterStatus } from "~/scripts/letter";
 
-export class Word {
-  readonly letters: Letter[] = []
 
-  get text() {
-    return this.letters.map((f) => f.char).join('')
-  }
+export class word{
+    readonly letters: Letter[] = [];
+    readonly maxWordLength = 5;
 
-  addLetter(char: string) {
-    if (this.letters.length < 5) {
-      this.letters.push(new Letter(char))
+    get text(){
+        return this.letters.map(x => x.char).join('');
     }
-  }
 
-  removeLetter() {
-    if (this.letters.length > 0) {
-      this.letters.pop()
-    }
-  }
-
-  evaluateWord(word: string): boolean {
-    let result = true
-
-    if (word.length === this.letters.length) {
-      const wordLettersLeft = word.split('')
-      const lettersLeft = []
-
-      for (const [index, letter] of this.letters.entries()) {
-        if (word[index] === letter.char) {
-          letter.status = LetterStatus.Correct
-          wordLettersLeft.splice(wordLettersLeft.indexOf(letter.char), 1)
-        } else {
-          result = false
-          lettersLeft.push(letter)
+    addLetter(char: string){
+        if(this.letters.length < this.maxWordLength){
+            this.letters.push(new Letter(char));
         }
-      }
-
-      if (!result) {
-        for (const letter of lettersLeft) {
-          if (wordLettersLeft.includes(letter.char)) {
-            letter.status = LetterStatus.WrongPlace
-            wordLettersLeft.splice(wordLettersLeft.indexOf(letter.char), 1)
-          } else {
-            letter.status = LetterStatus.Wrong
-          }
-        }
-      }
-      return result
-    } else {
-      return false
     }
-  }
 
-  get length() {
-    return this.letters.length
-  }
+    removeLetter(){
+        if(this.letters.length > 0){
+        this.letters.pop()
+        }
+    }
+
+    evaluateWord(word:string): boolean{
+        let result = true;
+
+        if(word.length === this.letters.length){
+            const wordLettersLeft = word.split('')
+            for(const[index,letter] of this.letters.entries()){
+                if(word[index] === letter.char){
+                    letter.status = LetterStatus.Correct
+                    wordLettersLeft.splice(wordLettersLeft.indexOf(letter.char),1)
+
+                }else if (wordLettersLeft.includes(letter.char)){
+                    letter.status = LetterStatus.WrongPlace
+                    wordLettersLeft.splice(wordLettersLeft.indexOf(letter.char),1)
+
+                    result = false
+                }else{
+                    letter.status = LetterStatus.Wrong;
+                    result = false
+                }
+
+            }
+
+            return result;
+        
+        }else{
+            return false;
+        }
+    
+    }
 }
