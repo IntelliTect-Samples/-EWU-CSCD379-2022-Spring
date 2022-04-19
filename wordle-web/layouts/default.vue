1bacfe58c3995690d9c601abffb52f9cfea701b7
BenagainB <91215159+BenagainB@users.noreply.github.com>
2022-04-18 22:58:07 -0700

    Started work on Ass2

--- wordle-web/layouts/default.vue
+++ wordle-web/layouts/default.vue
@@ -1,3 +1,11 @@
+<!-- Work needed on this page
+Add a settings dialog ❌✅
+Add a settings selection to the App Bar's menu with a gear icon. This should open this dialog regardless of where you are on the site ❌✅
+Option for dark and light mode ❌✅
+Develop two additional color schemes (with creative names) and allow the user to change to them. Schemes should look good in both light and dark mode ❌✅
+The above two items must be implemented with built in Veutify features ❌✅
+-->
+
 <template>
   <v-app dark>
     <v-navigation-drawer
@@ -26,17 +34,34 @@
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <!--
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
+      -->
+      <!--
       <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      -->
+      
       <v-toolbar-title v-text="title" />
+      
       <v-spacer />
+
+      <v-btn icon on-hover="light mode" @click.stop="fixed = !fixed" >
+        <v-icon> mdi-brightness-5 </v-icon>
+      </v-btn>
+
+      </v-btn>
+      <v-btn icon on-hover="dark mode" @click.stop="fixed = !fixed">
+        <v-icon> mdi-brightness-2 </v-icon>
+      </v-btn>
+
+      <v-btn icon @click.native.stop="dialog = true">
+        <v-icon> mdi-cog </v-icon>
+      </v-btn>
+
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
@@ -54,6 +79,20 @@
           </v-list-item-action>
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
+
+
+        <v-list-item :key="i" link @click="$router.push({ path: item.route })">
+          <v-list-item-action>
+            <v-icon light> mdi-trademark </v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              About
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+
       </v-list>
     </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
@@ -86,11 +125,25 @@ export default {
           title: 'Wordle',
           to: '/game',
         },
+        {
+          icon: 'mdi-trademark', 
+          title: 'About', 
+          to: '/about',
+        },
       ],
+/*
+      item: [
+        {
+          icon: 'mdi-trademark', 
+          title: 'About', 
+          to: '/about',
+        }
+      ],
+*/
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'Ben Burbank & Hewr Tarkhany',
     }
   },
 }
--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,3 +1,14 @@
+<!-- Work needed on this page:
+Create a custom component to show available words ❌✅
+Add a question mark key to the keyboard. This represents a placeholder for any character ❌✅
+Create/extend a word list component with a method called validWords that takes a string with question marks and returns an array of valid words ❌✅
+Create unit tests for the validWords method ❌✅
+Display the number of valid words based on the entered letters ❌✅
+When the number is clicked, show a dialog with all the available words with scrolling if there are too many to show on the screen. ❌✅
+When a word is clicked, the word should be entered as the guess but not submitted ❌✅
+Add a hover over for the word so it is obvious that it can be clicked. (No CSS) ❌✅
+-->
+
 <template>
   <v-container fluid fill-height justify-center>
     <v-tooltip bottom>
@@ -21,6 +32,9 @@
     <game-board :wordleGame="wordleGame" />
 
     <keyboard :wordleGame="wordleGame" />
+
+    
+
   </v-container>
 </template>
 
@@ -51,7 +65,7 @@ export default class Game extends Vue {
     }
     return { type: '', text: '' }
   }
-  
+
   getLetter(row: number, index: number) {
     const word: Word = this.wordleGame.words[row - 1]
     if (word !== undefined) {
