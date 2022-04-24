7b92cfa7dfcfe76a50aea62bfce83eb60c1cdf2d
Grant Erickson <grant@granterickson.com>
2022-04-24 14:38:58 -0700

    Delete Wordle.api directory

--- Wordle.api/Services/ILeaderBoardService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Wordle.Api.Services
-{
-    public interface ILeaderBoardService
-    {
-        IEnumerable<Score> GetHighScores();
-        Score UpdateScore(string name, int numberOfAttempts);
-    }
-}
--- Wordle.api/Services/JwtConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Wordle.Api.Services
-{
-    public class JwtConfiguration
-    {
-        public string? Secret { get; set; }
-        public string? Issuer { get; set; }
-    }
-}
