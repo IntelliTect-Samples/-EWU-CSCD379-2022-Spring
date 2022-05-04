96431ffac67fba960314165fa44669fad2c76c3a
Tyler Rose <trose8@ewu.edu>
2022-05-04 15:32:16 -0700

    Start Leaderboards Page

---
+++ wordle-web/pages/leaderboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fluid fill-height justify-center>
+
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component({})
+export default class Game extends Vue {
+  scoreNames: string[] = [];
+  scoreTimes: number[] = [];
+
+
+  getScores(){
+
+  }
+
+}
+</script>
