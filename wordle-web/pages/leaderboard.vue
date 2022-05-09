24e5bc4ddcf8dcfbdeeb3d9784f167c38e6aa6d4
A-Tanner <atanner5@ewu.edu>
2022-05-09 09:50:35 -0700

    Leaderboard gets top 10 from database

--- wordle-web/pages/leaderboard.vue
+++ wordle-web/pages/leaderboard.vue
@@ -1,21 +1,18 @@
 <template>
   <v-container fluid fill-height justify-center>
-    <v-row justify="center" dense no-gutters>
-      <v-col v-for="(i) in scoreNames" :key="i" cols="1" dense no-gutters>
+    <v-col justify="center" dense no-gutters>
+      <v-row v-for="(i) in scores" :key="i" cols="1" dense no-gutters>
         <v-container class="text-center mx-0 px-1 py-1 my-0">
           <v-card-text>
-            {{ i }}
+            {{ i.playerID }}
+            {{ i.name }}
+            {{ i.gameCount }}
+            {{ i.averageAttempts }}
+            {{ i.averageSecondsPerGame}}
           </v-card-text>
         </v-container>
-      </v-col>
-      <v-col v-for="(i) in scoreTimes" :key="i" cols="1" dense no-gutters>
-        <v-container class="text-center mx-0 px-1 py-1 my-0">
-          <v-card-text>
-            {{ i }}
-          </v-card-text>
-        </v-container>
-      </v-col>
-    </v-row>
+      </v-row>
+    </v-col>
     <v-card-actions>
       <v-btn color="primary" nuxt to="/game"> Back to the Game </v-btn>
     </v-card-actions>
@@ -28,10 +25,18 @@ import axios from "axios";
 
 @Component({})
 export default class ScoreBoard extends Vue {
-  scoreNames: string[] = [];
-  scoreTimes: number[] = [];
+  scores: any[] = [];
 
-
+      mounted(){
+        this.getTopTen();
+      }
+      
+      getTopTen(){
+        this.$axios.get('/api/Player')
+        .then(response => {
+            this.scores = response.data;
+        })
+    }
 
 }
 </script>
