08f655a755012e2d46ec7403b8340a9e512fcbf1
Tyler Rose <trose8@ewu.edu>
2022-05-10 01:35:00 -0700

    Leaderboard formatting update

--- wordle-web/components/leaderboardTile.vue
+++ wordle-web/components/leaderboardTile.vue
@@ -1,5 +1,31 @@
 <template>
-<v-card>
-    <slot/>
-</v-card>
+  <v-card>
+    <slot name="icon"></slot>
+    <v-row>
+      <v-col>
+        <v-card-text>{{ player.name }}</v-card-text>
+      </v-col>
+      <v-col>
+        <v-card-text>{{ player.gameCount }}</v-card-text>
+      </v-col>
+      <v-col>
+        <v-card-text>{{ player.averageAttempts }}</v-card-text>
+      </v-col>
+      <v-col>
+        <v-card-text>{{ player.averageSecondsPerGame }}</v-card-text>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
+
+<script lang="ts">
+import {Component, Vue, Prop} from 'vue-property-decorator'
+import {Player} from "~/scripts/player";
+
+@Component({components: {}})
+export default class GameBoard extends Vue {
+  @Prop({required: true})
+  player!: Player;
+
+}
+</script>
--- wordle-web/pages/leaderboard.vue
+++ wordle-web/pages/leaderboard.vue
@@ -1,17 +1,18 @@
 <template>
   <v-container fluid fill-height justify-center>
-    <v-card width = 80%>
-    <v-col justify="center" dense no-gutters>
-      <v-row v-for="player in scores" :key="player" dense no-gutters>
-        <leaderboardTile> {{player}} </leaderboardTile>
-          <!-- <v-col v-for="data in player" :key="data" align="right">
-          {{data}}
-          </v-col> -->
-      </v-row>
-    </v-col>
-    <v-card-actions>
-      <v-btn color="primary" nuxt to="/game"> Back to the Game </v-btn>
-    </v-card-actions>
+    <v-card width=80%>
+      <v-col justify="center" dense no-gutters>
+        <v-row v-for="(player, i) in scores" :key="player" dense no-gutters>
+          <leaderboardTile :data="player">
+            <slot name="icon">
+              <v-icon v-if="i === 1">mdi-crown</v-icon>
+            </slot>
+          </leaderboardTile>
+        </v-row>
+      </v-col>
+      <v-card-actions>
+        <v-btn color="primary" nuxt to="/game"> Back to the Game</v-btn>
+      </v-card-actions>
     </v-card>
   </v-container>
 </template>
@@ -19,22 +20,21 @@
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator'
 import {Player} from "~/scripts/player"
-import axios from "axios";
 
 @Component({})
 export default class ScoreBoard extends Vue {
   scores: Player[] = [];
 
-      mounted(){ 
-        this.getTopTen();
-      }
-      
-      getTopTen(){
-        this.$axios.get('/api/Player')
-        .then(response => {
-            this.scores = response.data;
-        })
-    }
+  mounted() {
+    this.getTopTen();
+  }
+
+  getTopTen() {
+    this.$axios.get('/api/Player')
+      .then(response => {
+        this.scores = response.data;
+      })
+  }
 
 }
 </script>
